5d98b2963eada31f80e6137f29c3ed3bbb2d7bd6
Noirifir <Augusto_L2K49@hotmail.com>
2023-07-26 11:13:49 -0300

    changes

--- resources/views/Augusto.blade.php
+++ resources/views/Augusto.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Ñaravell</title>
+        <title>Ñaravel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
