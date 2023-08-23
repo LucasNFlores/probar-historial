a9062a4b793d329c1e79b58edad2082be3ad5b0b
Noirifir <Augusto_L2K49@hotmail.com>
2023-08-23 09:02:43 -0300

    favicon , icon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,13 +9,15 @@
     <meta name="description" content="description here">
     <meta name="keywords" content="keywords,here">
 
-    <link rel="icon" href="public\images\logo.png" type="image/png">
+    <link rel="shortcut icon"   href="{{ asset('favicon.ico') }}">
+    <link rel="shortcut icon" sizes="192x192" href="{{ asset('favicon.ico') }}">
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/> <!--Replace with your tailwind.css once created-->
     <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet"> <!--Totally optional :) -->
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js" integrity="sha256-xKeoJ50pzbUGkpQxDYHD7o7hxe0LaOGeguUidbq6vis=" crossorigin="anonymous"></script>
-    
+
     <title>@yield('title')</title>
 
 
