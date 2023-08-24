615761a1b1c913f75c9f74f6bdaaf6b406ef2ff9
dariomarquez-cpu <dario.marquez@gmail.com>
2023-08-24 11:42:53 -0300

    Cerre el div del _sidebar

--- resources/views/base/_sidebar.blade.php
+++ resources/views/base/_sidebar.blade.php
@@ -2,7 +2,6 @@
         <div class="flex flex-col md:flex-row">
             <nav aria-label="alternative nav">
                 <div class="bg-gray-800 shadow-xl h-20 fixed bottom-0 mt-12 md:relative md:h-screen z-10 w-full md:w-48 content-center">
-
                     <div class="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
                         <ul class="list-reset flex flex-row md:flex-col pt-3 md:py-3 px-1 md:px-2 text-center md:text-left">
                             <li class="mr-3 flex-1">
@@ -22,9 +21,7 @@
                             </li>
                         </ul>
                     </div>
-
-
                 </div>
             </nav>
-            
+        </div>
 
