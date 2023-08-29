ffbfcf993fe6f262df39278b6f544dadf175e81e
Noirifir <Augusto_L2K49@hotmail.com>
2023-08-29 09:19:10 -0300

    crud-icons, mensajes

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -5,15 +5,17 @@
     <div class="  p-6">
         <!--Table Card-->
         <div class="bg-white border-transparent rounded-lg shadow-xl">
-            <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase flex justify-between text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                <h2 class="font-bold uppercase text-gray-600">Listado</h2>
+            <div class=" bg-gradient-to-b from-gray-300 to-gray-100 uppercase flex  justify-between   text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
+                <h2 class="font-bold uppercase text-gray-600 ">Listado  </h2>
+                <a href="{{route('createdevice')}}" class="opacity-50 hover:opacity-100  ">
+                    <span class="material-icons cursor-pointer " title="Agregar">add</span></a>
             </div>
 
     <div class="p-5">
 
         <table class="w-full text-sm text-left text-gray-700 uppercase ">
             <thead class="">
-                <tr class="  border-transparent rounded-lg shadow-xl">
+                <tr class="  border-transparent rounded-lg  ">
                     <th scope="col" class="px-6 py-3 text-center text-blue-900">Id</th>
                     <th scope="col" class="px-6 py-3 text-center text-blue-900">Nombre </th>
                     <th scope="col" class="px-6 py-3 text-center text-blue-900">Funcionalidad </th>
@@ -31,11 +33,11 @@
 
                     <td class="text-center "><a href="{{route('edit', $device->id)}}">
                         <form action="{{route('destroy', $device->id)}}" method="POST">
-                        <a href="{{route('createdata')}}" class="opacity-50 hover:opacity-100">Agregar<span class="material-icons cursor-pointer " >add</span></a>
+                        <a href="{{route('createdevice')}}" class="opacity-50 hover:opacity-100"><span class="material-icons cursor-pointer " title="Agregar">add</span></a>
                             @csrf
                             @method('DELETE')
-                            <input class="box-content cursor-pointer   material-icons " type="submit" value="delete"   onclick="return confirm('Está por eliminar el dato: {{$device->name}}')">
-                        <span class="material-icons cursor-pointer">edit</span> </a></td>
+                            <input class="box-content cursor-pointer   material-icons opacity-50 hover:opacity-100" title="Eliminar" type="submit" value="delete"   onclick="return confirm('Está por eliminar el dato: {{$device->name}}')">
+                        <span class="material-icons cursor-pointer opacity-50 hover:opacity-100" title="Editar">edit</span> </a></td>
                     </form>
                 </tr>
             @endforeach
