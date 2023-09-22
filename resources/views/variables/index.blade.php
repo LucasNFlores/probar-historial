963772fee1fa68674da2587d59a141e0f92944ce
Noirifir <Augusto_L2K49@hotmail.com>
2023-09-22 11:21:51 -0300

    light mode in rapasoft table and format

--- resources/views/variables/index.blade.php
+++ resources/views/variables/index.blade.php
@@ -2,9 +2,15 @@
 
     @section('title', 'Variables')
     @section('content')
-
+    <div class="  p-6">
+        <div class="bg-gradient-to-b from-gray-300 to-gray-100 uppercase flex justify-between text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
+            <h2 class="font-bold uppercase text-gray-600" flex-shrink-0>Listado</h2>
+            <a href="{{route('variables.create')}}" class="font-bold uppercase text-gray-600" flex-shrink-0><span class="material-icons cursor-pointer " title="Agregar">add</span></a></a>
+        </div>
+        <div class="pt-4">
 
     <livewire:variable-table />
-
+        </div>
+    </div>
 @endsection
 
