1ae0850aca66092c606aaa1b091aee205a3bfc00
Noirifir <Augusto_L2K49@hotmail.com>
2023-10-02 10:29:33 -0300

    fix user  actions,routes users

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,7 +4,7 @@
 
     @section('content')
         <div class="px-10 pt-12">
-            <form action="{{route('user.update',$user->id)}}" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <form action="{{route('users.update',$user->id)}}" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 @method('put')
                 @include('users._form',['botonNombre'=>'Actualizar','botonFondo'=>'bg-orange-500','botonFondoHover'=>'hover:bg-orange-700'])
             </form>
