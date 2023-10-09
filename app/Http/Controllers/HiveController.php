cfec504e44883ab122debf48cc596ae6b9dc8872
Noirifir <Augusto_L2K49@hotmail.com>
2023-10-08 21:43:32 -0300

    add cards in dashboard

--- app/Http/Controllers/HiveController.php
+++ app/Http/Controllers/HiveController.php
@@ -33,6 +33,8 @@ class HiveController extends Controller
             //se recupera el user autenticado y traemos todos los roles y pasamos de coleccion a array
             $arrayRoles = auth()->user()->roles->pluck('name')->toArray();
 
+            $userCount = 0;
+            $deviceCount = 0;
             $apiariesCount = 0;
             $hivesCount = 0;
             $userApiaries = [];
@@ -44,6 +46,8 @@ class HiveController extends Controller
 
                 foreach ($userApiaries as $apiary) {
                     $hives = $apiary->hives;
+
+                    $hives = $apiary->hives()->with('device')->get();
 
                     if ($hives instanceof \Illuminate\Support\Collection) {
                         // Si hives es una colección, incrementa el contador
@@ -62,9 +66,80 @@ class HiveController extends Controller
             return view('dashboard', [
                 'userApiaries' => $userApiaries,
                 'apiariesCount' => $apiariesCount,
-                'hivesCount' => $hivesCount
+                'hivesCount' => $hivesCount,
+                'userCount' => $userCount,
+                'deviceCount' => $deviceCount,
             ]);
         }
+
+/*  se intento busca  todos los dispositivos y recorrer para comparar por el id de hive que traiga del usuario para aumentar el contador
+
+            foreach ($hives as $hive) {
+                // Encuentra los dispositivos asociados a este hive
+                $devices = Device::where('hive_id', $hive->id)->get();
+
+                // Incrementa el contador de dispositivos
+                $deviceCount += $devices->count();
+            }
+        }
+*/
+/*  probando otra forma de traer
+
+                public function dashboard()#EN CADA METODO REVISAR QUE ESTEN BIEN LAS RUTAS
+                {
+                    //dd(auth()->user()::class::hasRole('Admin'));
+                    //dd(auth()->user()->roles->pluck('name')->first()); for else preguntar
+                    $user = auth()->user();
+                     //se recupera el user autenticado y traemos todos los roles y pasamos de coleccion a array
+                    $arrayRoles = auth()->user()->roles->pluck('name')->toArray();
+
+                    $userCount = 0;
+                    $deviceCount = 0;
+                    $apiariesCount = 0;
+                    $hivesCount = 0;
+                    $userApiaries = [];
+
+                    if (in_array('User', $arrayRoles)) {
+                        $id = $user->id;
+                        $userApiaries = Apiary::where('user_id', $id)->get();
+
+                        foreach ($userApiaries as $apiary) {
+                            $apiariesCount++;
+
+                            $hives = $apiary->hives()->with('devices')->get();
+
+                            if ($hives instanceof \Illuminate\Support\Collection) {
+                                $hivesCount += $hives->count();
+
+                                foreach ($hives as $hive) {
+                                    $devices = $hive->devices;
+                                  // Si hives es una colección, incrementa el contador
+                                    if ($devices instanceof \Illuminate\Support\Collection) {
+                                        $deviceCount += $devices->count();
+                                    }
+                                } // Si hives es un entero, úsalo directamente
+                            } elseif (is_int($hives)) {
+                                $hivesCount += $hives;
+                            }
+                        }
+                    } elseif (in_array('Admin', $arrayRoles)) {
+                        // Contar todos
+                        $apiariesCount = Apiary::count();
+                        $hivesCount = Hive::count();
+                        $userCount = User::count();
+                        $deviceCount = Device::count();
+                    }
+
+                    return view('dashboard', [
+                        'userApiaries' => $userApiaries,
+                        'apiariesCount' => $apiariesCount,
+                        'hivesCount' => $hivesCount,
+                        'userCount' => $userCount,
+                        'deviceCount' => $deviceCount,
+                    ]);
+                }
+
+*/
 
 
         //para que Ulises y Lucas tengan como estaba antes
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,60 @@
 @section('content')
     <div class="flex flex-wrap  ">
         <!--Estadisticas tarjetas-->
+
         <div class="w-full md:w-1/2 xl:w-1/3 p-6">
             <!--Metric Card-->
+            <div class="bg-gradient-to-b from-yellow-200 to-yellow-100 border-b-4  shadow-xl p-5">
+                <div class="flex flex-row items-center">
+                    <div class="flex-shrink pr-4">
+                        <div class="rounded-full p-5 bg-yellow-600"><i class="fas fa-solid fa-users fa-2x fa-inverse"></i></div>
+                    </div>
+                    <div class="flex-1 text-right md:text-center">
+                        <h2 class="font-bold uppercase text-gray-600">TOTAL DE USUARIOS</h2>
+                        <p class="font-bold text-3xl">{{$userCount}}</p>
+                    </div>
+                </div>
+            </div>
+            <!--/Metric Card-->
+        </div>
+
+        <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+            <!--Metric Card-->
+
+            <div class="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4  shadow-xl p-5">
+                <div class="flex flex-row items-center">
+                    <div class="flex-shrink pr-4">
+                        <div class="rounded-full p-5 bg-blue-600"><i class="fas fa-server fa-2x fa-inverse"></i></div>
+                    </div>
+                    <div class="flex-1 text-right md:text-center">
+                        <h2 class="font-bold uppercase text-gray-600"> VACIO</h2>
+                        <p class="font-bold text-3xl"> 0</p>
+                    </div>
+                </div>
+            </div>
+            <!--/Metric Card-->
+        </div>
+
+        <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+            <!--Metric Card-->
+
+            <div class="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4  shadow-xl p-5">
+                <div class="flex flex-row items-center">
+                    <div class="flex-shrink pr-4">
+                        <div class="rounded-full p-5 bg-blue-600"><i class="fas fa-server fa-2x fa-inverse"></i></div>
+                    </div>
+                    <div class="flex-1 text-right md:text-center">
+                        <h2 class="font-bold uppercase text-gray-600">VACIO</h2>
+                        <p class="font-bold text-3xl">0</p>
+                    </div>
+                </div>
+            </div>
+            <!--/Metric Card-->
+        </div>
+
+        <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+            <!--Metric Card-->
+
             <div class="bg-gradient-to-b from-blue-200 to-blue-100 border-b-4  shadow-xl p-5">
                 <div class="flex flex-row items-center">
                     <div class="flex-shrink pr-4">
@@ -18,6 +70,7 @@
             </div>
             <!--/Metric Card-->
         </div>
+
         <div class="w-full md:w-1/2 xl:w-1/3 p-6">
             <!--Metric Card-->
             <div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-500   shadow-xl p-5">
@@ -59,7 +112,7 @@
                     </div>
                     <div class="flex-1 text-right md:text-center">
                         <h2 class="font-bold uppercase text-gray-600">TOTAL DE DISPOSITIVOS</h2>
-                        <p class="font-bold text-3xl">{{$hivesCount}}</p>
+                        <p class="font-bold text-3xl">{{$deviceCount}}</p>
                     </div>
                 </div>
             </div>
